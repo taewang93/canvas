e5975d80b93b57edb2ae6226d92ca740cc270cff
kim taewang <taewang@nportverse.com>
2024-04-11 16:05:45 +0900

    fixed chart

--- chart/js/chart/random.js
+++ chart/js/chart/random.js
@@ -1,9 +1,10 @@
-import { Line } from "./line.js";
-import { Chart } from "./chart.js";
+import { Line } from "../index/line.js";
+import { Chart } from "./randomChart.js";
 
 class App {
   constructor() {
-    this.canvas = document.createElement("canvas");
+    // this.canvas = document.createElement("canvas");
+    this.canvas = document.querySelector("#chart");
     this.ctx = this.canvas.getContext("2d");
 
     document.body.appendChild(this.canvas);
@@ -11,7 +12,7 @@ class App {
     window.addEventListener("resize", this.resize.bind(this), false);
     this.resize();
 
-    // window.requestAnimationFrame(this.animate.bind(this));
+    window.requestAnimationFrame(this.animate.bind(this));
 
     this.line = new Line(this.stageWidth, this.stageHeight, 3);
     this.chart = new Chart(this.stageWidth, this.stageHeight, 2);
@@ -29,15 +30,19 @@ class App {
   }
 
   draw(ctx) {
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
     this.line.draw(ctx);
     this.chart.draw(ctx);
   }
 
-  // animate(t) {
-  //   window.requestAnimationFrame(this.animate.bind(this));
+  animate(t) {
+    window.requestAnimationFrame(this.animate.bind(this));
 
-  //   this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-  // }
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    this.line.draw(this.ctx);
+    this.chart.draw(this.ctx);
+  }
 }
 
 window.onload = () => {
--- chart/js/chart/randomChart.js
+++ chart/js/chart/randomChart.js
@@ -1,23 +1,49 @@
-const DATA = [4, 8, 6, 2, 7, 5, 9, 3, 10, 1];
+const DATA = [4, 8, 6, 2, 7, 5, 9, 3, 10];
+// const DATA = [];
+
+// setInterval(() => {
+//   const newData = Math.floor(Math.random() * 11);
+//   if (DATA.length > 9) DATA.splice(0, 1);
+//   DATA.push(newData);
+// }, 1000);
 
 export class Chart {
   constructor(width, height, lineWidth) {
     this.width = width;
     this.height = height;
     this.lineWidth = lineWidth;
+
+    this.data = [];
+    this.dataLength = this.data.length;
+
+    setInterval(() => {
+      const newData = Math.floor(Math.random() * 11);
+      if (this.data.length > 9) DATA.splice(0, 1);
+      this.data.push(newData);
+      console.log(this.data);
+    }, 1000);
   }
 
   draw(ctx) {
     ctx.strokeStyle = "#4285F4";
     ctx.lineWidth = this.lineWidth;
 
-    ctx.moveTo(0, this.height);
-    for (let i = 0; i < DATA.length; i++) {
-      ctx.lineTo(this.width * (i + 1) * 0.1, this.height * DATA[i] * 0.1);
+    ctx.moveTo(this.width, this.height);
+    for (let i = 0; i < this.dataLength; i++) {
+      const fixedI = 10 - i;
+      ctx.lineTo(
+        this.width * fixedI * 0.1,
+        this.height * this.data[this.dataLength - i - 1] * 0.1
+      );
     }
+    if (this.dataLength < 10) {
+      ctx.lineTo(this.width * (10 - this.dataLength) * 0.1, this.height);
+    }
+
     ctx.stroke();
+    if (this.dataLength == 10) ctx.lineTo(0, this.height);
     ctx.lineTo(this.width, this.height);
-    ctx.lineTo(0, this.height);
+    // ctx.lineTo(0, this.height);
     ctx.fillStyle = "rgba(66, 103, 210, 0.17)";
 
     ctx.fill();
